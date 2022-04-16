690fd46d7bc332350ec3924ce3a2db2ae979c199
BlueDragon-Safex <96148873+BlueDragon-Safex@users.noreply.github.com>
2022-04-16 11:07:38 -0400

    Add files via upload
    updates

--- setup.sh
+++ setup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-# wget -c -P /home/scripts https://raw.githubusercontent.com/BlueDragon-Safex/SafexNodeAutoSetup/4d962b2af49a4fb85ca093b8f1611f896c0a548c/setup.sh
+# wget -c -P /home/scripts http://70.34.242.65/setup.sh
 #chmod +x /home/scripts/setup.sh
 # /home/scripts/setup.sh
 
 mkdir -p /home/safex/lmdb/
 mkdir -p /home/scripts/
-sudo ufw allow 17402
-sudo ufw allow 17401
+ufw allow 17402
+ufw allow 17401
 
 
 # Get the community aliases
-wget -c -P /root https://raw.githubusercontent.com/BlueDragon-Safex/SafexNodeAutoSetup/main/.bash_aliases
+wget -c -P /root http://70.34.242.65/.bash_aliases
 
 #activate
-source ~/.bash_alaiases
-
+source ~/.bash_aliases
 
 #Get the manin script to run
-wget -c -P /home/scripts https://raw.githubusercontent.com/BlueDragon-Safex/SafexNodeAutoSetup/main/node.sh
+wget -c -P /home/scripts http://70.34.242.65/node.sh
 
 #activate it as a script
 chmod +x /home/scripts/node.sh
@@ -29,13 +28,13 @@ wget -c -P /home/safex/lmdb http://70.34.242.65/data.mdb
 sudo apt-get update
 apt-get install htop --assume-yes
 apt-get install screen --assume-yes
-apt-get install libnorm1 libpcsclite1 --assume-yes
-sudo apt update
+sudo apt install libnorm1 libpcsclite1 --assume-yes
+sudo apt update  
 
-apt-get install snapd --assume-yes
-sudo snap install docker
+sudo apt install snapd 
+sudo snap install docker 
 
-ln -s /snap/bin/docker /usr/bin/docker
+ln -s /snap/bin/docker /usr/bin/docker  
 
 
 ./home/scripts/node.sh
