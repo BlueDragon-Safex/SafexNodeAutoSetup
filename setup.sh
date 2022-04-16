79f72c09039c70584d4e3f634426e57e62f3c002
BlueDragon-Safex <96148873+BlueDragon-Safex@users.noreply.github.com>
2022-04-16 10:38:47 -0400

    Update setup.sh
    fixed spacing

--- setup.sh
+++ setup.sh
@@ -5,13 +5,12 @@
 
 mkdir -p /home/safex/lmdb/
 mkdir -p /home/scripts/
-ufw allow 17402
-ufw allow 17401
+sudo ufw allow 17402
+sudo ufw allow 17401
 
 
 # Get the community aliases
-wget -c -P /root
-https://raw.githubusercontent.com/BlueDragon-Safex/SafexNodeAutoSetup/main/.bash_aliases
+wget -c -P /root https://raw.githubusercontent.com/BlueDragon-Safex/SafexNodeAutoSetup/main/.bash_aliases
 
 #activate
 source ~/.bash_alaiases
@@ -27,16 +26,16 @@ chmod +x /home/scripts/node.sh
 wget -c -P /home/safex/lmdb http://70.34.242.65/data.mdb
 
 
-apt-get update
+sudo apt-get update
 apt-get install htop --assume-yes
 apt-get install screen --assume-yes
 apt-get install libnorm1 libpcsclite1 --assume-yes
-apt update  
+sudo apt update
 
 apt-get install snapd --assume-yes
-snap install docker 
+sudo snap install docker
 
-ln -s /snap/bin/docker /usr/bin/docker  
+ln -s /snap/bin/docker /usr/bin/docker
 
 
 ./home/scripts/node.sh
