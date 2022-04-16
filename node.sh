64436ee7a819d0e30b0ff2c663a2966c7a55b4ac
BlueDragon-Safex <96148873+BlueDragon-Safex@users.noreply.github.com>
2022-04-16 09:51:37 -0400

    node.sh
    This gets Public IP and starts Docker Image on reboot

---
+++ node.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Hashtags(#) will be the  start of a comnent line, not code. 
+
+# 1. The Path to save data to. Default is:  /home/safex
+dir='/home/safex'
+
+
+# 2. The version of the Docker Image/Safex Image. Default is:  7.0.2
+ver='7.0.2'
+
+
+# 3A or 3B (Use only one), put a hashtag before the ip mthod 
+#          you do not want to use
+# 3A. Hardcoded IP (enter your static IP here if desired)
+#ip='1.1.1.1'
+
+# 3b. Automatic IP (Recommended Method). This will lookup your IP Address automatically no matter which server.
+ip=$(dig @resolver4.opendns.com myip.opendns.com +short)
+
+
+#  4. This will be the command we use to lauch the Docker image and start Node
+#     The substitutions are in place. $dir and $ip and $ver are inside this line
+cmd="docker run -it --network=host --rm -v $dir:/data -e RPC_IP=$ip safexninja/safex-rpc-node:$ver"
+
+
+# 5. Before running, display what data we are using, such as our IP address
+#    and the command we are launching with
+echo "****************************************************************"
+echo
+echo "This server public IP is:  $ip"
+echo
+echo "Safex Node via Docker version: $ver using: "
+echo "$cmd"
+echo
+echo "Let's get this Safe party started!!"
+echo
+echo "****************************************************************"
+
+# 6. This will actually run the command and start the node. 
+$cmd
